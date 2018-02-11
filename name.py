a811f85f25b5ad211529a2c6da3397730458681d
Egan-eagle <36365585+Egan-eagle@users.noreply.github.com>
2018-02-11 14:33:57 +0000

    Update name.py

--- name.py
+++ name.py
@@ -25,3 +25,23 @@ print("My surname is", name)
 print("The first letter", first, "is number", firstno)
 print("The last letter", last, "is number", lastno)
 print("Fibonacci number", x, "is", ans)
+
+
+WEEK1
+
+# Egan Mthembu 24.01.2018
+# A program that displays Fibonacci numbers.
+
+def fib(n):
+  i = 0
+  j = 1
+  n = n - 1
+
+  while n >= 0:
+    i, j = j, i + j
+    n = n - 1
+  
+  return i
+x = 19
+ans = fib(x)
+print("Fibonacci number", x, "is", ans)
